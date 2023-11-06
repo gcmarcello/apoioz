c5b01e4c15cdbd07a7b868a3dad5b52fd7e6a5cb
Fgc17 <fgcoelho1709@outlook.com>
2023-11-06 15:08:27 +0000

    chore: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,26 +7,14 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   extends: ["next/core-web-vitals"],
-  plugins: [
-    /* "@typescript-eslint/eslint-plugin" */
-  ],
+  plugins: ["no-front-services"], // Ensure the plugin name is correct
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: [".eslintrc.js"],
-  overrides: [
-    {
-      files: ["**/(frontend)/**"],
-      rules: {
-        "no-restricted-imports": [
-          "error",
-          {
-            patterns: ["**/*service"],
-          },
-        ],
-      },
-    },
-  ],
+  rules: {
+    "no-front-services/no-front-services": "error",
+  },
 };
