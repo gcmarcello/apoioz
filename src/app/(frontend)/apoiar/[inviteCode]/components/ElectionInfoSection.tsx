10fa6d6778d288b207db1a928e2b6106116c1a3a
gcmarcello <marcellogcoelho@outlook.com>
2023-12-08 12:31:07 +0000

    Add password length validation to input field

--- src/app/(frontend)/apoiar/[inviteCode]/components/ElectionInfoSection.tsx
+++ src/app/(frontend)/apoiar/[inviteCode]/components/ElectionInfoSection.tsx
@@ -139,7 +139,7 @@ export function ElectionInfoSection({
               <input
                 type={showPassword ? "text" : "password"}
                 autoComplete="password"
-                {...form.register("password", { required: true })}
+                {...form.register("password", { required: true, minLength: 6 })}
                 id="password"
                 className="block w-full rounded-none rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
