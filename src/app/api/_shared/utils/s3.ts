8f6ce3d28195650aab23ff3dd38cc6594481fa82
gcmarcello <marcellogcoelho@outlook.com>
2023-12-21 23:29:56 +0000

    Refactor AWS credentials in S3 utility

--- src/app/api/_shared/utils/s3.ts
+++ src/app/api/_shared/utils/s3.ts
@@ -1,11 +1,18 @@
 import { getEnv } from "@/_shared/utils/settings";
 import { S3Client } from "@aws-sdk/client-s3";
 
+const accessKeyId = getEnv("AWS_ACCESS_KEY_ID");
+const secretAccessKey = getEnv("AWS_SECRET_ACCESS_KEY");
+
+if (!accessKeyId || !secretAccessKey) {
+  throw new Error("AWS credentials not found");
+}
+
 const s3Client = new S3Client({
   region: getEnv("AWS_REGION"),
   credentials: {
-    accessKeyId: getEnv("AWS_ACCESS_KEY_ID"),
-    secretAccessKey: getEnv("AWS_SECRET_ACCESS_KEY"),
+    accessKeyId,
+    secretAccessKey,
   },
 });
 
