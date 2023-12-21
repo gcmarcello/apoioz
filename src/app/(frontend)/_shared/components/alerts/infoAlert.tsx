210d320d5ffc4987b98a2aa452e09682356432fa
Fgc17 <fgcoelho1709@outlook.com>
2023-12-21 20:57:38 +0000

    Fix prop type in InfoAlert and format code in Select component

--- src/app/(frontend)/_shared/components/alerts/infoAlert.tsx
+++ src/app/(frontend)/_shared/components/alerts/infoAlert.tsx
@@ -1,6 +1,6 @@
 import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
-export function InfoAlert({ children }) {
+export function InfoAlert({ children }: { children: React.ReactNode }) {
   return (
     <div className="mb-4 flex text-sm text-gray-600">
       <InformationCircleIcon className="me-1 h-5 w-5" />
