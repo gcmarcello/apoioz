a25d609f57af4c96f69ecec52fde33dbe1a553a4
gcmarcello <marcellogcoelho@outlook.com>
2023-12-22 19:21:59 +0000

    Fix phone validation and add temporary any type

--- src/app/api/user/dto.ts
+++ src/app/api/user/dto.ts
@@ -6,14 +6,16 @@ export const createUserDto = z.object({
   name: z.string().min(3, { message: "Nome deve ter no mínimo 3 caracteres" }),
   email: z.string().email({ message: "Email inválido" }).min(3),
   password: z.string().nullable().optional(),
-  phone: z.custom(phoneValidator, { message: "Telefone inválido" }),
+  phone: z.custom(phoneValidator as any, { message: "Telefone inválido" }),
   info: z.object({
     zoneId: z.string().uuid().optional(),
     sectionId: z.string().uuid().optional(),
-    birthDate: z.custom(birthDateValidator, {
+    birthDate: z.custom(birthDateValidator as any, {
       message: "Data de nascimento inválida",
     }),
   }),
 });
+
+//@todo Temporary any until Zod accepts our pull request
 
 export type CreateUserDto = z.infer<typeof createUserDto>;
