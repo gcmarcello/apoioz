a8b346cff24e885dda39ab174a3dfa88bb248a6c
gcmarcello <marcellogcoelho@outlook.com>
2023-12-28 06:36:15 +0000

    Add Logo component to NewPasswordForm

--- src/app/(frontend)/(auth)/recuperar/[recoveryCode]/components/NewPasswordForm.tsx
+++ src/app/(frontend)/(auth)/recuperar/[recoveryCode]/components/NewPasswordForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Logo } from "@/app/(frontend)/_shared/components/Logo";
 import { ButtonSpinner } from "@/app/(frontend)/_shared/components/Spinners";
 import { showToast } from "@/app/(frontend)/_shared/components/alerts/toast";
 import { useAction } from "@/app/(frontend)/_shared/hooks/useAction";
@@ -45,11 +46,7 @@ export default function NewPasswordForm({
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center bg-white px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-          alt="Your Company"
-        />
+        <Logo />
 
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-700">
           Olá {resetInfo.userName}!
