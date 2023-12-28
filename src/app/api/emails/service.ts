314ae5f8d759b26f3181d89be3fe642d950f6108
gcmarcello <marcellogcoelho@outlook.com>
2023-12-28 05:32:43 +0000

    Refactor template file path in service.ts

--- src/app/api/emails/service.ts
+++ src/app/api/emails/service.ts
@@ -56,15 +56,7 @@ async function readEmailTemplate(templateId: string, dynamicData: { subject: str
 }
 
 async function readTemplateFile(templateId: string): Promise<string> {
-  const templatePath = path.join(
-    process.cwd(),
-    "src",
-    "app",
-    "api",
-    "emails",
-    "templates",
-    `${templateId}.html`
-  );
+  const templatePath = path.join("templates", `${templateId}.html`);
   const templateContent = await fs.readFile(templatePath, "utf8");
   return templateContent;
 }
