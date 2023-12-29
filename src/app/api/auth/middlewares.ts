44730016211860827c3d96e5a9b09e657145220f
gcmarcello <marcellogcoelho@outlook.com>
2023-12-29 10:01:41 +0000

    normalize email before login

--- src/app/api/auth/middlewares.ts
+++ src/app/api/auth/middlewares.ts
@@ -1,10 +1,13 @@
+import { normalizeEmail } from "@/_shared/utils/format";
 import { LoginDto } from "./dto";
 
 export async function ExistingUserMiddleware({ request }: { request: LoginDto }) {
   const isEmail = request.identifier.includes("@");
 
   const user = await prisma.user.findFirst({
-    where: isEmail ? { email: request.identifier } : { name: request.identifier },
+    where: isEmail
+      ? { email: normalizeEmail(request.identifier) }
+      : { name: request.identifier },
   });
 
   if (!user) throw `Usuário não encontrado`;
